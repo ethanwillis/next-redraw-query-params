e0ec8a85746b72e443dacab312c84c78474fbac7
Ethan Willis <elwillis1@gmail.com>
2020-02-11 14:54:36 -0600

    check if it redraws on non root paths

---
+++ pages/foo/bar/index.js
@@ -0,0 +1,13 @@
+import IncrementQueryParamCountButton from "../../../components/increment-query-param-count-button";
+import PageTitle from "../../../components/page-title";
+
+function Index() {
+  return (
+    <div>
+      <PageTitle />
+      <IncrementQueryParamCountButton />
+    </div>
+  )
+}
+
+export default Index;
